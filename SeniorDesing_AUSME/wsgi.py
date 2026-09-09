02666e858f4818272161b5dbc42ce52a4e1cd6fc
Kaidon Eccles <eccleskaidon@gmail.com>
2026-09-09 14:41:20 -0500

    Initial commit

---
+++ SeniorDesing_AUSME/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SeniorDesing_AUSME project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeniorDesing_AUSME.settings')
+
+application = get_wsgi_application()
