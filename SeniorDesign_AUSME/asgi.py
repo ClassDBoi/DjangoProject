da375f8b5ab413084c32d3d0ce72501f3c8e49fe
Kaidon Eccles <eccleskaidon@gmail.com>
2026-09-09 14:42:12 -0500

    Initial commit

--- SeniorDesign_AUSME/asgi.py
+++ SeniorDesign_AUSME/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SeniorDesing_AUSME project.
+ASGI config for SeniorDesign_AUSME project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeniorDesing_AUSME.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeniorDesign_AUSME.settings')
 
 application = get_asgi_application()
--- SeniorDesign_AUSME/wsgi.py
+++ SeniorDesign_AUSME/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SeniorDesing_AUSME project.
+WSGI config for SeniorDesign_AUSME project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeniorDesing_AUSME.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeniorDesign_AUSME.settings')
 
 application = get_wsgi_application()
